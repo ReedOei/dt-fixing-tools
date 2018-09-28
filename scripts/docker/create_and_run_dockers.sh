43fdb3afc0f7a612674e91dd1f3e36bf97f06b01
August Shi <awshi2@illinois.edu>
2018-09-28 17:28:48 -0500

    Fixing capitalization

--- scripts/docker/create_and_run_dockers.sh
+++ scripts/docker/create_and_run_dockers.sh
@@ -7,20 +7,21 @@ fi
 
 projfile=$1
 
-# Create base docker image
+# Create base Docker image
 docker build -t detectorbase:latest - < baseDockerfile
 
-# For each project,sha, make a docker image for it
+# For each project,sha, make a Docker image for it
 for line in $(cat ${projfile}); do
-    # Create the corresponding docker file
+    # Create the corresponding Dockerfile
     slug=$(echo ${line} | cut -d',' -f1)
     sha=$(echo ${line} | cut -d',' -f2)
     ./create_dockerfile.sh ${slug} ${sha}
 
-    # Build the docker image
+    # Build the Docker image
     modifiedslug=$(echo ${slug} | sed 's;/;.;')
     image=detector-${modifiedslug}:latest
     docker build -t ${image} - < ${modifiedslug}_Dockerfile
 
-    # Run the docker image
+    # Run the Docker image
+    # TBD
 done
