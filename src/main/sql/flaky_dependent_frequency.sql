fb35f4a5ca92016e5098e7f868ca166d3f05915b
Reed Oei <oei.reed@gmail.com>
2018-10-04 15:37:53 -0500

    Cleaned up query

--- src/main/sql/flaky_dependent_frequency.sql
+++ src/main/sql/flaky_dependent_frequency.sql
@@ -1,9 +1,9 @@
-select subject.name,
-	   subject.slug,
-	   detection_round.round_type,
-	   count(*) as n
-from subject
-inner join detection_round on detection_round.subject_name = subject.name
-inner join flaky_test_list on flaky_test_list.flaky_test_list_id = detection_round.filtered_id
-inner join flaky_test on flaky_test_list.flaky_test_id = flaky_test.id
-group by subject.name, subject.slug, detection_round.round_type;
+select s.name,
+	     s.slug,
+	     dr.round_type,
+	     count(*) as n
+from subject as s
+inner join detection_round as dr on dr.subject_name = s.name
+inner join flaky_test_list as ftl on ftl.flaky_test_list_id = dr.filtered_id
+inner join flaky_test as ft on ftl.flaky_test_id = ft.id
+group by s.name, s.slug, dr.round_type;
