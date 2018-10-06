09603e293c4058d955aa1e7ced8d42a4a5f3dbb6
August Shi <awshi2@illinois.edu>
2018-10-06 18:00:17 -0500

    Changing to allow for number of rounds to be passed in, adding more flags for skipping rat and batch, messing with directory permissions

--- scripts/docker/run_experiment.sh
+++ scripts/docker/run_experiment.sh
@@ -3,12 +3,14 @@
 # This script is the entry point script that is run inside of the Docker image
 # for running the experiment for a single project
 
-if [[ $1 == "" ]]; then
+if [[ $1 == "" ]] || [[ $2 == "" ]]; then
     echo "arg1 - GitHub SLUG"
+    echo "arg2 - Number of rounds"
     exit
 fi
 
 slug=$1
+rounds=$2
 
 date
 
@@ -16,9 +18,10 @@ date
 su - awshi2 -c "/home/awshi2/dt-fixing-tools/scripts/docker/update.sh"
 
 # Start the run_project.sh script using the awshi2 user
-su - awshi2 -c "/home/awshi2/dt-fixing-tools/scripts/docker/run_project.sh ${slug}"
+su - awshi2 -c "/home/awshi2/dt-fixing-tools/scripts/docker/run_project.sh ${slug} ${rounds}"
 
 # Change permissions of results and copy outside the Docker image (assume outside mounted under /Scratch)
 modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
-chown -R $(id -u):$(id -g) /home/awshi2/output/
 cp -r /home/awshi2/output/ /Scratch/${modifiedslug}_output/
+chown -R $(id -u):$(id -g) /Scratch/${modifiedslug}_output/
+chmod -R 777 /Scratch/${modifiedslug}_output/
--- scripts/docker/run_project.sh
+++ scripts/docker/run_project.sh
@@ -3,12 +3,14 @@
 # This script is run inside the Docker image, for single experiment (one project)
 # Should only be invoked by the run_experiment.sh script
 
-if [[ $1 == "" ]]; then
+if [[ $1 == "" ]] || [[ $2 == "" ]]; then
     echo "arg1 - GitHub SLUG"
+    echo "arg2 - Number of rounds"
     exit
 fi
 
 slug=$1
+rounds=$2
 
 # Setup prolog stuff
 cd /home/awshi2/dt-fixing-tools/scripts/
@@ -22,8 +24,9 @@ cd /home/awshi2/${slug}
 /home/awshi2/dt-fixing-tools/scripts/docker/pom-modify/modify-project.sh .
 
 # Run the plugin
-/home/awshi2/apache-maven/bin/mvn testrunner:testplugin -Denforcer.skip=true -fn |& tee log
+/home/awshi2/apache-maven/bin/mvn testrunner:testplugin -Denforcer.skip=true -Drat.skip=true -Ddt.randomize.rounds=${rounds} -fn -B |& tee log
 
 # Gather the results, put them up top
 mkdir /home/awshi2/output/
 /home/awshi2/dt-fixing-tools/scripts/gather-results $(pwd) /home/awshi2/output/
+mv log /home/awshi2/output/
