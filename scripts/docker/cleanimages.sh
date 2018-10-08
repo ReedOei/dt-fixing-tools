ba143985a108e7037dee5c68a857f16a584e74dc
Wing Lam <winglam2@asedl.cs.illinois.edu>
2018-10-08 10:21:42 -0500

    Clean images only clean what is necessary now.

--- scripts/docker/cleanimages.sh
+++ scripts/docker/cleanimages.sh
@@ -13,13 +13,14 @@ for line in $(cat ${projfile}); do
     modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
     image=detector-${modifiedslug}:latest
     docker rmi ${image} 2> /dev/null
+
+    rm -rf ${modifiedslug}_Dockerfile
+    rm -rf ${modifiedslug}_output/
 done
 
 # Delete the base tooling
 docker rmi toolingdetectorbase:latest 2> /dev/null
 
 
-rm -rf *_Dockerfile
-rm -rf *_output/
 rm -rf run_output.log
 rm -rf nohup.out
