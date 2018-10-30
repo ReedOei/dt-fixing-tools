ae9a266b14799bafc5c3aa679da116ae412eb14d
Reed Oei <oei.reed@gmail.com>
2018-10-30 11:31:05 -0500

    Log more things, make process more robust

--- src/main/java/edu/illinois/cs/dt/tools/diagnosis/DiagnoserPathManager.java
+++ src/main/java/edu/illinois/cs/dt/tools/diagnosis/DiagnoserPathManager.java
@@ -6,9 +6,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class DiagnoserPathManager extends PathManager {
+    public static final Path SUBJECT_PROPERTIES = Paths.get("subject.properties");
     public static Path DIAGNOSIS = Paths.get("diagnosis");
 
     public static Path diagnosisResult(final DiagnosisResult diagnosisResult) {
         return path(DIAGNOSIS).resolve(diagnosisResult.minimized().dependentTest() + "-" + diagnosisResult.minimized().expected() + ".json");
+    }
+
+    public static Path subjectProperties() {
+        return path(SUBJECT_PROPERTIES);
     }
 }
--- src/main/java/edu/illinois/cs/dt/tools/diagnosis/DiagnoserPlugin.java
+++ src/main/java/edu/illinois/cs/dt/tools/diagnosis/DiagnoserPlugin.java
@@ -13,18 +13,26 @@ import edu.illinois.cs.dt.tools.minimizer.MinimizerPathManager;
 import edu.illinois.cs.dt.tools.minimizer.MinimizerPlugin;
 import edu.illinois.cs.dt.tools.runner.InstrumentingSmartRunner;
 import edu.illinois.cs.dt.tools.runner.RunnerListener;
+import edu.illinois.cs.dt.tools.utility.PathManager;
 import org.apache.maven.project.MavenProject;
 import scala.Option;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -32,6 +40,7 @@ public class DiagnoserPlugin extends TestPlugin {
     private MavenProject project;
     private Path javaAgent;
     private InstrumentingSmartRunner runner;
+    private String coordinates;
 
     public DiagnoserPlugin() {
     }
@@ -44,14 +53,53 @@ public class DiagnoserPlugin extends TestPlugin {
                       .collect(Collectors.toList()));
     }
 
+    private void writeError(final Throwable t) {
+        try {
+            System.out.println("---------------------------------------------------");
+            System.out.println("ERROR (WRITE_ERROR_STDOUT_THROWABLE_REEDOEI2): " + coordinates);
+            t.printStackTrace();
+
+            t.printStackTrace(new PrintStream(new FileOutputStream(String.valueOf(DetectorPathManager.errorPath()))));
+            Files.write(DetectorPathManager.errorPath(), ("\n" + coordinates).getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.out.println("ERROR (FAIL_OUTPUT_ERR_THROWABLE_REEDOEI2): Failed to output error!");
+            e.printStackTrace();
+            System.out.println("---------------------------------------------------");
+            t.printStackTrace();
+        }
+    }
+
+    private void writeError(final String msg) {
+        try {
+            System.out.println("---------------------------------------------------");
+            System.out.println("ERROR (WRITE_ERROR_STDOUT_STRING_REEDOEI2): " + coordinates);
+            System.out.println("Message was:");
+            System.out.println(msg);
+
+            Files.write(DetectorPathManager.errorPath(), (msg + "\n").getBytes());
+            Files.write(DetectorPathManager.errorPath(), ("\n" + coordinates).getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.out.println("ERROR (FAIL_OUTPUT_ERR_STRING_REEDOEI2): Failed to output error!");
+            e.printStackTrace();
+            System.out.println("---------------------------------------------------");
+            System.out.println("Message was:");
+            System.out.println(msg);
+        }
+    }
+
     @Override
     public void execute(final MavenProject project) {
         this.project = project;
+        this.coordinates = project.getGroupId() + ":" + project.getArtifactId() + ":" + project.getVersion();
+
+        System.out.println("DIAGNOSER_MODULE_COORDINATES: " + coordinates);
 
         this.javaAgent = Paths.get(Configuration.config().getProperty("dtfixingtools.javaagent", ""));
         final Option<Runner> runnerOption = RunnerFactory.from(project);
 
         try {
+            writeSubjectProperties();
+
             if (runnerOption.isDefined()) {
                 this.runner = InstrumentingSmartRunner.fromRunner(runnerOption.get());
 
@@ -69,15 +117,45 @@ public class DiagnoserPlugin extends TestPlugin {
             } else {
                 TestPluginPlugin.mojo().getLog().info("Module is not using a supported test framework (probably not JUnit)");
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Throwable t) {
+            writeError(t);
+        } finally {
+            final Path failingOutput = project.getBasedir().toPath().resolve("failing-test-output");
+            final Path failingOutputDest = DiagnoserPathManager.cachePath().resolve("failing-test-output");
+
+            if (Files.exists(failingOutput)) {
+                try {
+                    if (Files.exists(failingOutputDest)) {
+                        Files.copy(failingOutput, failingOutputDest, StandardCopyOption.REPLACE_EXISTING);
+                    } else {
+                        Files.copy(failingOutput, failingOutputDest);
+                    }
+                } catch (IOException ignored) {}
+            }
         }
+    }
+
+    private void writeSubjectProperties() throws IOException {
+        final Properties properties = new Properties();
+        properties.setProperty("subject.coordinates", coordinates);
+        properties.setProperty("subject.name", subjectName(project));
+
+        Files.createDirectories(DiagnoserPathManager.subjectProperties().getParent());
+        properties.store(new FileOutputStream(DiagnoserPathManager.subjectProperties().toFile()), "");
+    }
+
+    private String subjectName(final MavenProject project) {
+        final Path relativePath =
+                PathManager.parentPath().getParent().toAbsolutePath()
+                        .relativize(project.getBasedir().toPath().toAbsolutePath());
+
+        return relativePath.toString().replace("/", "-");
     }
 
     private void diagnose() throws Exception {
         results()
                 .map(result -> new TestDiagnoser(runner, result).run())
-                .flatMap(o -> o.isPresent() ? Stream.of(o.get()) : Stream.empty())
+                .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
                 .forEach(this::saveResult);
     }
 
