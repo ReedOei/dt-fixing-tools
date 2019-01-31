7d4b0c011008f19980e3fd26a19dc9e4d078c759
Reed Oei <oei.reed@gmail.com>
2019-01-31 16:18:53 -0600

    Remove unused scripts

--- scripts/generate_graphic.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Usage: bash generate_graphic.sh <DATABASE> <SUBJECT NAME>
-
-scripts_folder=$(cd "$(dirname $BASH_SOURCE)"; pwd)
-
-bash "$scripts_folder/list_flaky_runs.sh" "$1" "$2"
-"$scripts_folder"/../graphic
