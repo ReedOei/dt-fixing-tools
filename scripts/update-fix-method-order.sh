f417f1c66fc750a56e4571ab917548e978e4801c
Wing Lam <winglam2@asedl.cs.illinois.edu>
2019-02-02 18:07:01 -0600

    Added list of nod tests.

--- scripts/update-fix-method-order.sh
+++ scripts/update-fix-method-order.sh
@@ -10,18 +10,24 @@ fi
 database="$1"
 
 echo "[INFO] Updating FixMethodOrder info"
-QUERY="select s.slug, ftc.subject_name, ftc.test_name from flaky_test_classification ftc join subject s on ftc.subject_name = s.name;"
+# QUERY="select s.slug, ftc.subject_name, ftc.test_name from fixable_tests ftc join subject s on ftc.subject_name = s.name;"
+QUERY="select s.slug, ftc.subject_name, ftc.test_name, sr.sha from fixable_tests ftc join subject s on ftc.subject_name = s.name join subject_raw sr on sr.slug = s.slug;"
 lines=$(echo "$QUERY" | sqlite3 "$database")
 for line in $lines; do
     slug=$(echo $line | cut -d'|' -f1)
     subject_name=$(echo $line | cut -d'|' -f2)
     test_name=$(echo $line | cut -d'|' -f3)
+    sha=$(echo $line | cut -d'|' -f4)
     # Get everything before the last '.'
     test_class_name=$(echo $test_name | rev | cut -f2- -d"." | rev)
 
     if [[ ! -d "temp-subject/$slug" ]]; then
         mkdir -p "temp-subject"
         git clone "https://github.com/$slug" "temp-subject/$slug"
+        (
+            cd "temp-subject/$slug"
+            git checkout $sha
+        )
     fi
 
     if find "temp-subject/$slug" -name "*.java" | grep -E "$test_class_name.java$" | xargs cat | grep -q "@FixMethodOrder"; then
