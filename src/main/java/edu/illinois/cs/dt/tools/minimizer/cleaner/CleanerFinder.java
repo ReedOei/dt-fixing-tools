5306473cf1ab1bed60fa37ebc6d04b7cb5bd2db5
August Shi <awshi2@illinois.edu>
2019-02-05 15:51:54 -0600

    Adding import for TreeMap

--- src/main/java/edu/illinois/cs/dt/tools/minimizer/cleaner/CleanerFinder.java
+++ src/main/java/edu/illinois/cs/dt/tools/minimizer/cleaner/CleanerFinder.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 import java.util.stream.Stream;
 
 public class CleanerFinder {
