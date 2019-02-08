859c734e9779318e83cdedf19149beb12306b094
August Shi <awshi2@illinois.edu>
2019-02-07 22:36:03 -0600

    Initial hack at finding inherited test methods

--- src/main/java/edu/illinois/cs/dt/tools/fixer/JavaMethod.java
+++ src/main/java/edu/illinois/cs/dt/tools/fixer/JavaMethod.java
@@ -11,9 +11,14 @@ import com.github.javaparser.ast.stmt.TryStmt;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import org.apache.commons.io.FilenameUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,6 +34,47 @@ public class JavaMethod {
 
                 if (methodDeclaration != null) {
                     return Optional.of(new JavaMethod(methodName, javaFile, methodDeclaration));
+                }
+            }
+        }
+        // If cannot find the specific method within the file, it must be inherited from above
+        // Search for the method in a superclass
+        List<URL> urlList = new ArrayList();
+        for (String cp : classpath.split(":")) {
+            try {
+                urlList.add(new File(cp).toURL());
+            } catch (MalformedURLException mue) {
+                System.out.println("Classpath element " + cp + " is malformed!");
+            }
+        }
+        URL[] urls = urlList.toArray(new URL[urlList.size()]);
+        URLClassLoader projectClassLoader = URLClassLoader.newInstance(urls);
+        // Get a list of potential method names using the super classes
+        List<String> superMethodNames = new ArrayList<>();
+        String testClassName = methodName.substring(0, methodName.lastIndexOf('.'));
+        String baseMethodName = methodName.substring(methodName.lastIndexOf('.') + 1);
+        Class testClass;
+        try {
+            testClass = projectClassLoader.loadClass(testClassName);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Cannot load class " + testClassName);
+            return Optional.empty();
+        }
+        Class currClass = testClass;
+        while (currClass != null) {
+            superMethodNames.add(currClass.toString().replace("class ", "") + "." + baseMethodName);
+            currClass = currClass.getSuperclass();
+        }
+        for (String superMethodName : superMethodNames) {
+            for (final Path file : files) {
+                if (Files.exists(file) && FilenameUtils.isExtension(file.getFileName().toString(), "java")) {
+                    final JavaFile javaFile = JavaFile.loadFile(file, classpath, CleanerPathManager.compiledPath(file).getParent());
+
+                    final MethodDeclaration methodDeclaration = javaFile.findMethodDeclaration(superMethodName);
+
+                    if (methodDeclaration != null) {
+                        return Optional.of(new JavaMethod(superMethodName, javaFile, methodDeclaration));
+                    }
                 }
             }
         }
