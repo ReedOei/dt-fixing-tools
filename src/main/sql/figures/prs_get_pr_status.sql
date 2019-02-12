8705882206d949bc122ec90e96b4247fb4ab65d5
winglam <wing.c.lam@gmail.com>
2019-02-12 15:22:19 -0600

    Fixing bug in total for PRs not adding up.

--- src/main/sql/figures/prs_get_pr_status.sql
+++ src/main/sql/figures/prs_get_pr_status.sql
@@ -1,7 +1,8 @@
-select subject_name, count(subject_name) as pCount
+select pr.subject_name, count(pr.subject_name) as pCount
 from 
   (select pt.subject_name, pt.pr_link 
    from pr_tests pt 
    where pt.pr_status like ? 
-   group by pt.subject_name, pt.pr_link) 
-group by subject_name;
+   group by pt.subject_name, pt.pr_link) pr
+join od_classification oc on oc.subject_name = pr.subject_name
+group by pr.subject_name;
--- src/main/sql/figures/prs_get_pr_tests.sql
+++ src/main/sql/figures/prs_get_pr_tests.sql
@@ -1,4 +1,5 @@
-select subject_name, count(test_name) as tCount
-from pr_tests 
-where pr_status like ? 
-group by subject_name;
+select pt.subject_name, count(pt.test_name) as tCount
+from pr_tests pt
+join od_classification oc on oc.subject_name = pt.subject_name
+where pt.pr_status like ? 
+group by pt.subject_name;
