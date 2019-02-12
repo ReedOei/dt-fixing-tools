46172d7d2361d2a9a0763dd24ae7c4b6ed0f0d3c
winglam <wing.c.lam@gmail.com>
2019-02-11 18:05:38 -0600

    Fixing bugs in queries.

--- src/main/sql/figures/count_max_cleaner_victim.sql
+++ src/main/sql/figures/count_max_cleaner_victim.sql
@@ -3,6 +3,6 @@ from
   (select odc.test_name, count(cg.test_name) as num
   from od_classification odc
   left join cleaner_groups cg on odc.test_name = cg.test_name
-  where od_type = 'victim' and ifnull(cleaner_count, 0) >= 0 and ifnull(cleaner_count, 0) <= 100000
+  where od_type = 'victim' and ifnull(cleaner_count, 0) >= ? and ifnull(cleaner_count, 0) <= ?
   group by odc.test_name
-  having num >= 2 and num <= 100000);
+  having num >= ? and num <= ?);
--- src/main/sql/figures/count_max_polluter_victim.sql
+++ src/main/sql/figures/count_max_polluter_victim.sql
@@ -4,7 +4,7 @@ from
   from od_classification odc
   inner join dependency_groups dg on dg.test_name = odc.test_name
   where od_type like ? and
-        dg.dep_count >= 0 and
+        dg.dep_count >= ? and
         dg.dep_count <= ?
   group by odc.test_name
   having num >= ? and num <= ?);
