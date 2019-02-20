e70b89076a06fe81cd2252383a1490595986132b
August Shi <awshi2@illinois.edu>
2019-02-20 15:39:06 -0600

    Report numbers on a per test basis as well

--- scripts/find_unique_patches.sh
+++ scripts/find_unique_patches.sh
@@ -17,6 +17,10 @@ overallworkingpatchescount=0
 overallpossiblepatches=0
 overallpossiblepatchescount=0
 
+overalluniquepatchespertest=0
+overalluniquepatchessizespertest=0
+overalltestcount=0
+
 IFS=$'\n'
 for module in $(grep -v "#" ${testsfile} | cut -d',' -f2 | sort -u); do
     # Make a temporary file but manipulate it through a file descriptor, so it gets deleted regardless when script ends
@@ -27,12 +31,21 @@ for module in $(grep -v "#" ${testsfile} | cut -d',' -f2 | sort -u); do
     testcount=0
     pairscount=0
     count=0
+
+    rollingpertestunique=0
+    rollingpertestuniquesize=0
+
     for t in $(grep ",${module}" ${testsfile} | grep -v "#" | cut -d',' -f1); do
         d=$(find ${debuggingresults} -maxdepth 4 -name fixer.log | grep "=${t}/" | xargs -n1 dirname)
         # Count how many fixed dependent tests there are
         if [[ $(grep -r "INLINE" ${d}) != "" ]]; then
             testcount=$((testcount + 1))
         fi
+
+        # Make a temporary file but manipulate it through a file descriptor, so it gets deleted regardless when script ends
+        tmpfile=$(mktemp /tmp/find_unique_patches.XXXXXX)
+        exec 4> "${tmpfile}"
+        rm "${tmpfile}"
 
         # Count unique patches
         for p in $(find ${d} -name "*.patch*"); do
@@ -50,10 +63,19 @@ for module in $(grep -v "#" ${testsfile} | cut -d',' -f2 | sort -u); do
                 # Checking both patch contents and the name of the modified method
                 #echo $(md5sum <(echo "$(echo ${modified}) $(tail -n +$((linenum + 1)) ${p})") | cut -d' ' -f1) ${size} >>3
                 echo $(md5sum <(echo "$(tail -n +$((linenum + 1)) ${p})") | cut -d' ' -f1) ${size} >>3
+
+                # Count up per test
+                echo $(md5sum <(echo "$(tail -n +$((linenum + 1)) ${p})") | cut -d' ' -f1) ${size} >>4
             fi
         done
+        rollingpertestunique=$(echo "$(sort -u <4 | wc -l) + ${rollingpertestunique}" | bc -l)
+        rollingpertestuniquesize=$(echo "$(sort -u <4 | cut -d' ' -f2 | sort -u | wc -l) + ${rollingpertestuniquesize}" | bc -l)
+        echo -n "" >4
     done
+
     echo "\\Def{${module}_fixed_tests}{${testcount}}"
+
+    # Unique patches across every test in the module
     if [[ ${count} == 0 ]]; then
         echo "\\Def{${module}_unique_patches}{n/a}"
         echo "\\Def{${module}_unique_patches_sizes}{n/a}"
@@ -66,6 +88,8 @@ for module in $(grep -v "#" ${testsfile} | cut -d',' -f2 | sort -u); do
         overalluniquepatchessizes=$(echo "$(sort -u <3 | cut -d' ' -f2 | sort -u | wc -l) + ${overalluniquepatchessizes}" | bc -l)
         overalluniquepatchescount=$((overalluniquepatchescount + 1))
     fi
+
+    # Working patches across every test in the module
     if [[ ${count} == 0 ]]; then
         echo "\\Def{${module}_working_patches}{n/a}"
     else
@@ -73,12 +97,28 @@ for module in $(grep -v "#" ${testsfile} | cut -d',' -f2 | sort -u); do
         overallworkingpatches=$(echo "${overallworkingpatches} + ${count}" | bc -l)
         overallworkingpatchescount=$((overallworkingpatchescount + 1))
     fi
+
+    # Possible patches across every test in the module (should be the same as working...)
     if [[ ${pairscount} == 0 ]]; then
         echo "\\Def{${module}_possible_patches}{n/a}"
     else
         echo "\\Def{${module}_possible_patches}{${pairscount}}"
         overallpossiblepatches=$(echo "${overallpossiblepatches} + ${pairscount}" | bc -l)
         overallpossiblepatchescount=$((overallpossiblepatchescount + 1))
+    fi
+
+    # Unique patches per test, averaged across tests in this module
+    if [[ ${testcount} == 0 ]]; then
+        echo "\\Def{${module}_unique_patches_pertest}{n/a}"
+        echo "\\Def{${module}_unique_patches_sizes_pertest}{n/a}"
+    else
+        avgunique=$(echo "${rollingpertestunique} / ${testcount}" | bc -l)
+        echo "\\Def{${module}_unique_patches_pertest}{$(echo "${avgunique}" | xargs printf "%.1f")}"
+        avgsizeunique=$(echo "${rollingpertestuniquesize} / ${testcount}" | bc -l)
+        echo "\\Def{${module}_unique_patches_sizes_pertest}{$(echo "${avgsizeunique}" | xargs printf "%.1f")}"
+        overalluniquepatchespertest=$(echo "${rollingpertestunique} + ${overalluniquepatchespertest}" | bc -l)
+        overalluniquepatchessizespertest=$(echo "${rollingpertestuniquesize} + ${overalluniquepatchessizespertest}" | bc -l)
+        overalltestcount=$(echo "${overalltestcount} + ${testcount}" | bc -l)
     fi
     echo -n "" >3
 done
@@ -88,6 +128,10 @@ echo "\\Def{average_unique_patches}{$(echo ${overalluniquepatches} / ${overallun
 echo "\\Def{average_unique_patches_sizes}{$(echo ${overalluniquepatchessizes} / ${overalluniquepatchescount} | bc -l | xargs printf "%.1f")}"
 echo "\\Def{average_working_patches}{$(echo ${overallworkingpatches} / ${overallworkingpatchescount} | bc -l | xargs printf "%.1f")}"
 echo "\\Def{average_possible_patches}{$(echo ${overallpossiblepatches} / ${overallpossiblepatchescount} | bc -l | xargs printf "%.1f")}"
+
+echo "\\Def{total_testcount}{${overalltestcount}}"
+echo "\\Def{average_unique_patches_pertest}{$(echo ${overalluniquepatchespertest} / ${overalltestcount} | bc -l | xargs printf "%.1f")}"
+echo "\\Def{average_unique_patches_sizes_pertest}{$(echo ${overalluniquepatchessizespertest} / ${overalltestcount} | bc -l | xargs printf "%.1f")}"
 
 echo "\\Def{total_unique_patches}{${overalluniquepatches}}"
 echo "\\Def{total_working_patches}{${overallworkingpatches}}"
