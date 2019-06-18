d29a258a277fc50603670973356e49eae79c09b2
August Shi <awshi2@illinois.edu>
2019-06-18 13:41:50 -0500

    Some more tweaking to get it to output in format we want

--- scripts/gather-results.sh
+++ scripts/gather-results.sh
@@ -8,7 +8,21 @@ fi
 path=$1
 outputpath=$2
 
+modulekey() {
+    projroot=$1
+    moduledir=$2
+
+    # In case it is not a subdirectory, handle it so does not use the .
+    relpath=$(realpath $(dirname ${moduledir}) --relative-to ${projroot})
+    if [[ ${relpath} == '.' ]]; then
+        basename ${projroot}
+        return
+    fi
+
+    # Otherwise convert into expected format
+    echo $(basename ${projroot})-$(realpath $(dirname ${moduledir}) --relative-to ${projroot} | sed 's;/;-;g')
+}
+
 for d in $(find ${path} -name .dtfixingtools); do
-    modulekey=$(echo $(basename ${path})-$(realpath $(dirname ${d}) --relative-to ${path} | sed 's;/;-;g'))
-    cp -r ${d} ${outputpath}/${modulekey}
+    cp -r ${d} ${outputpath}/$(modulekey ${path} ${d})
 done
