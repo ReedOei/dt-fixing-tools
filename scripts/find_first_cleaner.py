0bbd3f78d1a2bbb37c5e7a4671443573e84e5b3d
August Shi <awshi2@illinois.edu>
2019-06-20 18:19:37 -0500

    Changing to report when cleaner was actually found

--- scripts/find_first_cleaner.py
+++ scripts/find_first_cleaner.py
@@ -14,15 +14,15 @@ def main(args):
     # If there are cleaners, we want the time spent looking for that first cleaner of the first polluter
     if len(cleanerData['cleaners']) > 0:
         for cleaner in cleanerData['cleaners']:
-            print cleaner['time']['totalTime']['elapsedSeconds']
+            print str(cleaner['time']['totalTime']['elapsedSeconds']) + ',SUCCESS'
     else:
         # If there is only one polluter, then it is the overall time, minus the time for the polluter
         if len(data['polluters']) == 1:
-            print data['time']['elapsedSeconds'] - firstpol['time']['elapsedSeconds']
+            print str(data['time']['elapsedSeconds'] - firstpol['time']['elapsedSeconds']) + ',FAIL'
         else:
             # Otherwise, need to grab time of second polluter and subtract the start of that with the end of the first
             secondpol = data['polluters'][1]
-            print (secondpol['time']['startTime'] - firstpol['time']['endTime']) / 1000.0
+            print str((secondpol['time']['startTime'] - firstpol['time']['endTime']) / 1000.0) + ',FAIL'
             #sys.stderr.write('WEIRD CASE, NEED TO HANDLE ' + jsonfile + '\n')
 
 if __name__ == '__main__':
