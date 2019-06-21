c079c645ff3c1fafd104abd5d4d4e9a2bb6db583
Wing Lam <winglam2@asedl.cs.illinois.edu>
2019-06-21 02:13:10 -0500

    Adding script to download docker images.

---
+++ scripts/docker/download-docker-image.sh
@@ -0,0 +1,21 @@
+if ! [ -x "$(command -v azcopy)" ]; then
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod/ xenial main" > azure.list
+    sudo cp -f ./azure.list /etc/apt/sources.list.d/
+    sudo apt-key adv --keyserver packages.microsoft.com --recv-keys EB3E94ADBE1229CF
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https
+    sudo apt-get update
+    sudo apt-get install -y azcopy
+fi
+
+line=$(cat $1)
+imageName=$(echo ${line} | cut -d',' -f4)
+
+azcopy \
+    --source https://dtdataset.file.core.windows.net/dtdata/$imageName \
+    --destination ./$imageName \
+    --source-key $2 \
+    --preserve-last-modified-time \
+    --quiet
+
+docker load -i $imageName
