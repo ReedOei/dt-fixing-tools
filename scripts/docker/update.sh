22f3132bd5bfa1336ab7f97bb6907001c3718e6a
Wing Lam <winglam2@asedl.cs.illinois.edu>
2019-09-30 17:42:53 -0500

    Setup to run experiments to get number of commits per test with no follow.

--- scripts/docker/update.sh
+++ scripts/docker/update.sh
@@ -3,4 +3,4 @@
 # Script that updates Git repositories of tooling code to latest and rebuilds
 
 cd /home/awshi2/testrunner/ && git pull && /home/awshi2/apache-maven/bin/mvn clean install -B
-cd /home/awshi2/dt-fixing-tools/ && git pull && /home/awshi2/apache-maven/bin/mvn clean install -B
+cd /home/awshi2/dt-fixing-tools/ && git pull && git checkout no-follow && /home/awshi2/apache-maven/bin/mvn clean install -B
