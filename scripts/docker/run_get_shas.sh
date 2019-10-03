2bfa4f787b34e334c0347c7bcf6213ab87be500a
Wing Lam <winglam2@asedl.cs.illinois.edu>
2019-10-03 18:49:14 -0500

    Fix for wildfly.

--- scripts/docker/run_get_shas.sh
+++ scripts/docker/run_get_shas.sh
@@ -44,21 +44,23 @@ RESULTSDIR=/home/awshi2/output/
 mkdir -p ${RESULTSDIR}
 
 
-first=$(git log -G "${testName}" --pretty=%H  | tail -1)
+for first in $(git log -G "${testName}" --pretty=%H  | tac); do
 file_name=$(git show ${first} | egrep "^\\+\+\+|${testName}" | grep -B1 "${testName}" | head -1 | rev | cut -b6- | cut -f1 -d/ | rev)
 
 echo "Suspected first commit is $first" > /home/awshi2/commits.log
 
-if [[ $file_name == $className ]]; 
+if [[ $file_name == $className ]];
 then
   echo "First commit does contain likely test file." >> /home/awshi2/commits.log
   count=$(($(git rev-list --count $first..$idflakiesSha) - 1))
   echo "Number of commits between first ($first) and iDFlakies ($idflakiesSha) commit: $count" >> /home/awshi2/commits.log
+  break
 else
   echo "First commit does not contain likely test file." >> /home/awshi2/commits.log
   echo "File name found is $file_name while className is $className" >> /home/awshi2/commits.log
-  git show ${first} >> /home/awshi2/commits.log
+  # git show ${first} >> /home/awshi2/commits.log
 fi
+done
 
 cp /home/awshi2/commits.log ${RESULTSDIR}
 
