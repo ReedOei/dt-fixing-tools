c991783e34e5a28cffafc675339a11a6aab59c25
Stefan Winter <sw@cs.tu-darmstadt.de>
2019-10-03 19:30:23 -0500

    fixed bug in the fail case logic, copy build log to outside world

--- scripts/docker/buildshas.sh
+++ scripts/docker/buildshas.sh
@@ -13,11 +13,15 @@ repo=$1
 sha=$2
 
 [ -d /home/awshi2/${sha} ] && exit 0
-[ -f /home/awshi2/${sha}/mvn-install-${sha}.log ] && exit 1
+[ -f /home/awshi2/mvn-install-${sha}.log ] && exit 1
 
 git clone ${repo} /home/awshi2/${sha}
 cd /home/awshi2/$sha
-timeout 1h /home/awshi2/apache-maven/bin/mvn clean install -DskipTests -fn -B |& tee /home/awshi2/${sha}/mvn-install-${sha}.log
+timeout 1h /home/awshi2/apache-maven/bin/mvn clean install -DskipTests -fn -B |& tee /home/awshi2/mvn-install-${sha}.log
+slug=$(echo ${line} | cut -d',' -f1 | rev | cut -d'/' -f1-2 | rev)
+modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
+[ -d /Scratch/${modifiedslug}=${sha}_build_output ] || mkdir /Scratch/${modifiedslug}=${sha}_build_output
+cp /home/awshi2/mvn-install-${sha}.log /Scratch/${modifiedslug}=${sha}_build_output/
 
 [ $? -ne 0 ] && { rm -rf /home/awshi2/${sha}; exit 1; }
 
