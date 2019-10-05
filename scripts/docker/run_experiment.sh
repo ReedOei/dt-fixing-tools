aab53d0754fa32774b0e04379db30c39c0e65b1f
winglam <wing.c.lam@gmail.com>
2019-10-05 18:29:15 -0500

    Improvements to how experiments are saved.

--- scripts/docker/run_experiment.sh
+++ scripts/docker/run_experiment.sh
@@ -3,32 +3,37 @@
 # This script is the entry point script that is run inside of the Docker image
 # for running the experiment for a single project
 
-if [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $3 == "" ]] || [[ $4 == "" ]]; then
+if [[ $1 == "" ]] || [[ $2 == "" ]] || [[ $3 == "" ]] [[ $4 ==  "" ]] || [[ $5 == "" ]]; then
     echo "arg1 - GitHub SLUG"
     echo "arg2 - Number of rounds"
     echo "arg3 - Timeout in seconds"
-    echo "arg4 - Test name (Optional)"
-    echo "arg5 - Script to run (Optional)"
-    echo "arg6 - Script arguments (Optional)"
+    echo "arg4 - Full sha of the initial project"
+    echo "arg5 - CSV input line for this run"
+    echo "arg6 - Test name (Optional)"
+    echo "arg7 - Script to run (Optional)"
+    echo "arg8 - Script arguments (Optional)"
     exit
-fi
-
-# If it's an absolute path, just use it
-if [[ "$5" =~ "$/" ]]; then
-    script_to_run="$4"
-elif [[ -z "$5" ]]; then
-    # The default is run_project.sh
-    script_to_run="/home/awshi2/dt-fixing-tools/scripts/docker/run_project.sh"
-else
-    # otherwise, assume it's relative to the docker directory
-    script_to_run="/home/awshi2/dt-fixing-tools/scripts/docker/$5"
-    script_args="$6"
 fi
 
 slug=$1
 rounds=$2
 timeout=$3
-testName=$4
+sha=$4
+line=$5
+testName=$6
+short_sha=${sha:0:7}
+
+# If it's an absolute path, just use it
+if [[ "$7" =~ "$/" ]]; then
+    script_to_run="$7"
+elif [[ -z "$7" ]]; then
+    # The default is run_project.sh
+    script_to_run="/home/awshi2/dt-fixing-tools/scripts/docker/run_project.sh"
+else
+    # otherwise, assume it's relative to the docker directory
+    script_to_run="/home/awshi2/dt-fixing-tools/scripts/docker/$7"
+    script_args="$8"
+fi
 
 git rev-parse HEAD
 date
@@ -55,6 +60,8 @@ su - awshi2 -c "$script_to_run ${slug} ${rounds} ${timeout} ${testName} ${script
 
 # Change permissions of results and copy outside the Docker image (assume outside mounted under /Scratch)
 modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
-cp -r /home/awshi2/output/ /Scratch/${modifiedslug}=${testName}_output/
+output_dir=/Scratch/${modifiedslug}-${short_sha}=${testName}_output/
+cp -r /home/awshi2/output/ $output_dir
+echo "$line" > $output_dir/input.csv
 chown -R $(id -u):$(id -g) /Scratch/${modifiedslug}=${testName}_output/
 chmod -R 777 /Scratch/${modifiedslug}=${testName}_output/
