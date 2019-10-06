7e4e557d1de6b6b1a81711c2596de18090e58401
winglam <wing.c.lam@gmail.com>
2019-10-06 15:52:38 -0500

    Fixing bug in ordering of sql columns.

--- src/main/java/edu/illinois/cs/dt/tools/analysis/Analysis.java
+++ src/main/java/edu/illinois/cs/dt/tools/analysis/Analysis.java
@@ -210,8 +210,8 @@ public class Analysis extends StandardMain {
     private void insertCommitOrderTestsHelp(final String testName, final String commitSha, final String orderNum) throws SQLException, IOException {
         // orderNum: Number of commits inbetween commitSha and the iDFlakies sha; -1 means this is the iDFlakies sha
         sqlite.statement(SQLStatements.INSERT_FS_TESTS_COMMIT_NUM)
-                .param(commitSha)
                 .param(testName)
+                .param(commitSha)
                 .param(orderNum)
                 .insertSingleRow();
     }
