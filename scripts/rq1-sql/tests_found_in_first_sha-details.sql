692498e9b9f2210ef0ada417dcb7d9c011402600
Wing Lam <winglam2@illinois.edu>
2019-10-12 01:26:44 +0200

    Fixing bug in sql queries.

--- scripts/rq1-sql/tests_found_in_first_sha-details.sql
+++ scripts/rq1-sql/tests_found_in_first_sha-details.sql
@@ -1,2 +1,2 @@
-select distinct ftf.subject_name,ftf.test_name,ftf.flaky_type,ftf.commit_sha
+select distinct ftf.slug,ftf.test_name,ftf.flaky_type,ftf.commit_sha
 from fs_rq1_tests_found_in_first_sha_details ftf;
