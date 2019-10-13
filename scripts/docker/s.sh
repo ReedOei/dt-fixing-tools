1076ffc2c92f7f1b8c26dad5e83f1c30adfacee1
Stefan Winter <sw@cs.tu-darmstadt.de>
2019-10-13 15:01:54 -0500

    fixed bug in number of files reporting in git log analysis script

--- scripts/docker/s.sh
+++ scripts/docker/s.sh
@@ -28,7 +28,7 @@ for first in $(git log -G "${testNameWithParen}" --pretty=%H  | tac); do
   for n in $(git show $first --numstat --format=oneline | sed -E '1d ; /^\s*$/d ; s/[[:blank:]]/ /g' | cut -d' ' -f1); do ((firstAddedLines += $n)); done
   firstRemovedLines=0
   for n in $(git show $first --numstat --format=oneline | sed -E '1d ; /^\s*$/d ; s/[[:blank:]]/ /g' | cut -d' ' -f2); do ((firstRemovedLines += $n)); done
-  echo "Files changed in first ($first) commit: $firstChangeFiles" >> $outputFile
+  echo "Files changed in first ($first) commit: $firstChangedFiles" >> $outputFile
   echo "Lines added in first ($first) commit: $firstAddedLines" >> $outputFile
   echo "Lines removed in first ($first) commit: $firstRemovedLines" >> $outputFile
   
