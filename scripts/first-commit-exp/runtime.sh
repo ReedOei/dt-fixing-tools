18ee76f4038721bf9446e22dc77e3dc997d170c2
Wing Lam <winglam2@illinois.edu>
2019-11-22 04:16:09 +0100

    Adding command to get runtime of experiments.

--- scripts/first-commit-exp/runtime.sh
+++ scripts/first-commit-exp/runtime.sh
@@ -1,2 +1,8 @@
 find -name "*_output-log.txt" |
     egrep -A 1 "(Starting|Finished) run_get_files_tests.sh" doanduyhai.achilles-ac85374\=info.archinnov.achilles.it.TestEntityWithSASIIndices.should_search_using_like_non_tokenizer_output-log.txt | tr -d '\r' | { readarray a ; echo $(( ( $(date +%s -d "$(echo ${a[4]} | tr -d '\n')") - $(date +%s -d "$(echo ${a[1]} | tr -d '\n')") ) / 60 )); }
+
+# run insides logs/
+for f in $(ls *-log.txt); do min=$(egrep -A 1 "(Starting|Finished) run_get_files_projs.sh" $f | tr -d '\r' | { readarray a ; echo $(( ( $(date +%s -d "$(echo ${a[4]} | tr -d '\n')") - $(date +%s -d "$(echo ${a[1]} | tr -d '\n')") ) / 60 )); }); hour=$(($min * 50 / 60)); name=$(echo $f | cut -d'=' -f1); echo "$name,$min,$hour"; done | sort -k3 -k2 -k1 -n -t ','
+
+
+
